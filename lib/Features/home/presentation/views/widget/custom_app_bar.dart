1655f08e1e72aa839ec0db502a78550e295f89e8
chat_app <mohamed_khaled329@yahoo.com>
2024-04-14 17:57:23 +0200

    create custom search text field

--- lib/Features/home/presentation/views/widget/custom_app_bar.dart
+++ lib/Features/home/presentation/views/widget/custom_app_bar.dart
@@ -1,20 +1,31 @@
+import 'package:bookly_app/core/utils/app%20router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal:  14.0,vertical: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 30),
       child: Row(
         children: [
-          Image.asset(AssetData.logo,height: 18,),
+          Image.asset(
+            AssetData.logo,
+            height: 18,
+          ),
           const Spacer(),
-          IconButton(onPressed: () {}, icon: const Icon(FontAwesomeIcons.magnifyingGlass,size: 28,)),
-      
+          IconButton(
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.ksearchView);
+              },
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 22,
+              )),
         ],
       ),
     );
--- lib/Features/search/presentation/views/search_view_body.dart
+++ lib/Features/search/presentation/views/search_view_body.dart
@@ -1,10 +1,19 @@
+import 'package:bookly_app/Features/search/presentation/views/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SearchviewBody extends StatelessWidget {
   const SearchviewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
   }
 }
