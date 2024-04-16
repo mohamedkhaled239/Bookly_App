44da77c673fb18138facfcf1adc95c43b020ac6e
chat_app <mohamed_khaled329@yahoo.com>
2024-04-16 19:08:07 +0200

    newest books cubit done

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/bookmodel/bookmodel.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+
+   final HomeRepo homeRepo;
+
+  Future<void> fetchNewstBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+        (failure) => emit(
+              NewsetBooksFailure(failure.errMessage),
+            ), (books) {
+      emit(
+        NewsetBooksSuccess(books),
+      );
+    });
+  }
 }
